35f8c67f2e0a1944d2096c1de010fc9f5dd836c2
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2024-05-13 21:32:21 -0300

    listo para entregar

--- clinicaUm/src/main/java/ar/edu/um/clinicaUm/MainApp.java
+++ clinicaUm/src/main/java/ar/edu/um/clinicaUm/MainApp.java
@@ -9,25 +9,42 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainApp {
+  private static final int NUM_THREADS = 10;
+  private static final int NUM_PATIENTS = 10;
+
   public static void main(String[] args) {
-    ObraSocialDto obraSocial = new ObraSocialDto("Obra Social 1", "OS1");
-    Map<Integer, ObraSocialDto> obrasSociales = new HashMap<>();
-    obrasSociales.put(obraSocial.hashCode(), obraSocial);
-    MedicoDto medico = new MedicoDto("John", "Doe", "1234", "General", true, obrasSociales);
-    MedicamentoDto medicamento =
-        new MedicamentoDto("Medicamento 1", "Droga 1", "Laboratorio 1", "Presentacion 1", "100");
-    Map<Integer, MedicamentoDto> medicamentos = new HashMap<>();
-    medicamentos.put(medicamento.hashCode(), medicamento);
-    RecetaDto receta = new RecetaDto(1, medicamentos);
+    MedicoDto medico = createMedico();
+    RecetaDto receta = createReceta();
 
-    ExecutorService executor = Executors.newFixedThreadPool(10);
+    ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
 
-    for (int i = 0; i < 10; i++) {
-      PacienteDto paciente = new PacienteDto("Jane" + i, "Doe", obraSocial, receta);
-
-      executor.submit(new PacienteSvc(paciente, medico));
+    for (int i = 0; i < NUM_PATIENTS; i++) {
+      PacienteDto paciente = createPaciente(i, medico, receta);
+      try {
+        executor.submit(new PacienteSvc(paciente, medico));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 
     executor.shutdown();
+  }
+
+  private static PacienteDto createPaciente(int id, MedicoDto medico, RecetaDto receta) {
+    return new PacienteDto("Patient " + id, "Doe", medico.obrasSocialesAcepatadas().values().iterator().next(), receta);
+  }
+
+  private static MedicoDto createMedico() {
+    ObraSocialDto obraSocial = new ObraSocialDto("Obra Social 1", "OS1");
+    Map<Integer, ObraSocialDto> obrasSociales = new HashMap<>();
+    obrasSociales.put(obraSocial.hashCode(), obraSocial);
+    return new MedicoDto("John", "Doe", "1234", "General", true, obrasSociales);
+  }
+
+  private static RecetaDto createReceta() {
+    MedicamentoDto medicamento = new MedicamentoDto("Medicamento 1", "Droga 1", "Laboratorio 1", "Presentacion 1", "100");
+    Map<Integer, MedicamentoDto> medicamentos = new HashMap<>();
+    medicamentos.put(medicamento.hashCode(), medicamento);
+    return new RecetaDto(1, medicamentos);
   }
 }
